2134eda921a59719c5fe2cdc984bcf524cbb2d7b
luokaiheng <739464015@qq.com>
2024-04-11 22:27:38 +0800

    新增代码

--- CMVIP05-TEST/class_unittest/unittest_demo.py
+++ CMVIP05-TEST/class_unittest/unittest_demo.py
@@ -26,10 +26,16 @@ class TestDemo(unittest.TestCase):
     def tearDown(self):
         print('----查看页面')
 
+    # 作为普通函数，是可以被写入测试用例里面的
+    def func_01(self):
+        print('把一堆我想要的东西，加入购物车')
+
     def test_01_Login(self):
         print('点击登录按钮')
 
     def test_02_Buy(self):
+        # 类内自己调用函数，使用 self.方法名 来调用
+        self.func_01()
         print('点击购买按钮')
 
     def test_03_cancel(self):
