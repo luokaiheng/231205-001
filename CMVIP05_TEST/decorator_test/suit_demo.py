957137617b604b641287835ca52d3333fd23079e
luokaiheng <739464015@qq.com>
2024-04-14 16:25:27 +0800

    新增代码

--- CMVIP05_TEST/decorator_test/suit_demo.py
+++ CMVIP05_TEST/decorator_test/suit_demo.py
@@ -12,9 +12,12 @@
             UnitTest.TestSuite中可以自定义添加测试用例执行
             套件运行是依托于运行器runner来运行的
 '''
+import os
 import unittest
 
 from CMVIP05_TEST.decorator_test.test_01 import Demo
+from HTMLTestRunner import HTMLTestRunner
+from HTMLTestReportCN import HTMLTestRunner
 
 # 创建套件
 suite = unittest.TestSuite()
@@ -31,8 +34,28 @@ suite = unittest.TestSuite()
 # suite.addTest(unittest.TestLoader().loadTestsFromName("test_01.Demo"))
 # 5.批量添加,在持续集成中和批量管理中特别好用
 path = "./"
-discover = unittest.defaultTestLoader.discover(start_dir=path,pattern='test*.py')
+discover = unittest.defaultTestLoader.discover(start_dir=path, pattern='test*.py')
 
-# 运行套件内的测试用例,默认运行器,verbosity是日志等级:0-1
-runner = unittest.TextTestRunner(verbosity=1)
-runner.run(discover)
+# 配置测试报告信息
+report_tester = 'kaiheng'
+# 保存路径
+report_dir = './report/'
+# 测试报告的title
+report_title = '凯恒的测试报告'
+# 描述
+report_description = '这是测试报告的描述'
+# 测试报告文件
+report_file = report_dir + 'report.html'
+# 如果没有该文件夹路径，则生成路径
+if not os.path.exists(report_dir):
+    os.mkdir(report_dir)
+
+# 生成HTMLTestRunner的测试报告
+with open(report_file, 'wb')as file:
+    runner = HTMLTestRunner(stream=file, title=report_title,
+                            description=report_description, verbosity=2, tester=report_tester)
+    runner.run(discover)
+
+# # 运行套件内的测试用例,默认运行器,verbosity是日志等级:0-1
+# runner = unittest.TextTestRunner(verbosity=1)
+# runner.run(discover)
