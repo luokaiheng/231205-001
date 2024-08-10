f7ab7443f4b75e53c629bd53476ee17cfd6efbd7
luokaiheng <739464015@qq.com>
2024-08-10 16:48:10 +0800

    data装饰器可以通过函数去操作文件

--- CMVIP05_TEST/DDT_yaml/test_yaml.py
+++ CMVIP05_TEST/DDT_yaml/test_yaml.py
@@ -6,14 +6,51 @@
 @time: 2024/6/8  15:59
 @software: PyCharm
 @desc:读取yaml文件的内容，open读取任何文件都可以，但是yaml格式，在open之后还需要额外的操作
-1.data 实现数据分离，data 的实现：
-    a.声明data 装饰器，生成对应的测试数据，每一组数据都基于","进行区分
-    b.基于data 分离了多少组数据，则该条用例执行多少次
-    c.基于测试函数中定义的形参，将data分离的每一组数据依次传入
-2.unpack 解包data中的数据包
-    a.基于data生成的数据组，进行二次解包
-    b.解包的规则和data是一样的
-    c.基于data和unpack装饰器解包的数据组，个数上要与形参完全一致
 '''
+import yaml
+
+import os
 
 
+def count_files_in_directory(directory_path):
+    file_count = 0
+    for entry in os.scandir(directory_path):
+        if entry.is_file():
+            file_count += 1
+    return file_count
+
+
+# 示例用法
+directory_path = './data'
+file_count = count_files_in_directory(directory_path)
+
+
+# print(f"The directory '{directory_path}' contains {file_count} files.")
+# print(file_count)
+
+li01 = []
+def list_files_in_directory(directory):
+    # 获取目录下的所有文件和子目录名
+    filenames = os.listdir(directory)
+
+    # 打印文件名
+    for filename in filenames:
+        li01.append(filename)
+        # print(filename)
+
+
+# 指定要列出文件的目录
+# list_files_in_directory(directory_path)
+
+
+def read_yaml():
+    for name in li01:
+        file_path = './data/' + name
+        file = open(file_path, 'r', encoding='utf_8', )
+        # 读取yaml中的内容，和之前学过的文件读取有所不同
+        data = yaml.load(stream=file, Loader=yaml.FullLoader)
+        print(data)
+
+
+if __name__ == '__main__':
+    read_yaml()
