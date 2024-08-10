cfdac1a76974c99969a1f6a3227e2e2dd761e1e4
luokaiheng <739464015@qq.com>
2024-08-10 15:05:52 +0800

    提交ddt 和yaml 代码

--- CMVIP05_TEST/DDT_yaml/test_DDT.py
+++ CMVIP05_TEST/DDT_yaml/test_DDT.py
@@ -2,10 +2,10 @@
 # encoding: utf-8
 '''
 @author: Loeb{luokaiheng}
-@file: test_DDT.py
-@time: 2024/6/8  14:49
+@file: test_yaml.py
+@time: 2024/6/8  15:59
 @software: PyCharm
-@desc:基于ddt实现Unittest测试用例与数据分离的形态
+@desc:读取yaml文件的内容，open读取任何文件都可以，但是yaml格式，在open之后还需要额外的操作
 1.data 实现数据分离，data 的实现：
     a.声明data 装饰器，生成对应的测试数据，每一组数据都基于","进行区分
     b.基于data 分离了多少组数据，则该条用例执行多少次
@@ -16,37 +16,48 @@
     c.基于data和unpack装饰器解包的数据组，个数上要与形参完全一致
 '''
 import unittest
+
 from ddt import ddt, data, unpack
 
-li = ['li01', 'li02', 'li03']
 
-
-@ddt()
+# ddt后面不要接括号
+@ddt
 class Demo(unittest.TestCase):
-    @data('li01', 'li02', 'li03')
-    def test_01(self, name):
-        print(name)
+    def test_01(self):
+        li = ['凯恒', '小凯', '恒儿', '小罗']
+        for n in li:
+            print(n + '，你好，欢迎来到这个世界')
+        # print('这是用例001')
 
-    @data(['li01', '这是一个list'], {'name': 'li02'}, 'li03')
+    # 声明调用data装饰器：将多组数据存放在data装饰器中，便于用例的多次调用
+    '''
+        1.data 实现数据分离，data 的实现：
+            a.声明data 装饰器，生成对应的测试数据，每一组数据都基于","进行区分
+            b.基于data 分离了多少组数据，则该条用例执行多少次
+            c.基于测试函数中定义的形参，将data分离的每一组数据依次传入
+    '''
+
+    @data('凯恒', '小凯', '恒儿', '小罗')
     def test_02(self, name):
-        print(name)
+        print(name + '，你好，欢迎来到这个中国')
+        # print('这是用例002')
 
-    @data(['申远', '陈树波'], ['林钊颖', '李华丽'], ['王华其', '莫朝辉'])
-    @unpack
-    def test_03(self, name, type_):
-        print(name)
-        print('*' * 20)
-        print(type_)
+    @data('string', ['list00', 'list01'], {'dict': 'value'})
+    def test_03(self, name):
+        if type(name) is list:
+            print(name, end="")
+            print('，列表可以作为参数传进来')
+        elif type(name) is dict:
+            print(name, end="")
+            print('，字典也ok')
+        else:
+            print(name + '，字符串也可')
 
-    # 传参报错
-    @data(['申远', '陈树波', '罗凯恒'], ['林钊颖', '李华丽'], ['王华其', '莫朝辉'])
+    @data(['list00', 'list01'], ['list02', 'list03'], ['list04', 'list05'], ['list06', 'list07', 'list08'])
     @unpack
-    def test_04(self, name, type_, type_0):
-        print(name)
-        print('*' * 20)
-        print(type_)
-        print('*' * 20)
-        print(type_0)
+    def test_04(self, name, type_, type_001='None'):
+        print('如果当测试用例中，需要传入多组形参数据，可以通过声明unpack装饰器来对数据组进行拆包')
+        print(name + ' ' + type_ + ' ' + type_001)
 
 
 if __name__ == '__main__':
