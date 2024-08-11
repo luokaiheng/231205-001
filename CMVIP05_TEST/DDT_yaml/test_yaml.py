07eb5ca9cabc0ff6405c0f407e6f05d3ed956fad
luokaiheng <739464015@qq.com>
2024-08-11 11:32:38 +0800

    yaml文件的读写操作

--- CMVIP05_TEST/DDT_yaml/test_yaml.py
+++ CMVIP05_TEST/DDT_yaml/test_yaml.py
@@ -11,46 +11,75 @@ import yaml
 
 import os
 
-
-def count_files_in_directory(directory_path):
-    file_count = 0
-    for entry in os.scandir(directory_path):
-        if entry.is_file():
-            file_count += 1
-    return file_count
+dict001 = {
+    "name": "罗凯恒",
+    "job": "软件测试"
+}
 
 
-# 示例用法
-directory_path = './data'
-file_count = count_files_in_directory(directory_path)
+class Read_Yaml():
+    '''
+        指定文件夹中的数据文件，限定文件格式为txt、yaml
+    '''
+    # 类属性，相当于全局变量
+    directory_path = './data'
+    li01 = []
+
+    def count_files_in_directory(self):
+        file_count = 0
+        for entry in os.scandir(Read_Yaml.directory_path):
+            if entry.is_file():
+                file_count += 1
+        print('这个文件目录下，有' + str(file_count) + '个文件' + '\n')
+        return file_count
+
+    def list_files_in_directory(self):
+        # 获取目录下的所有文件和子目录名
+        filenames = os.listdir(Read_Yaml.directory_path)
+
+        # 打印文件名
+        for filename in filenames:
+            Read_Yaml.li01.append(filename)
+
+    def read_yaml(self):
+        for name in Read_Yaml.li01:
+            file_path = './data/' + name
+            file = open(file_path, 'r', encoding='utf_8', )
+            # 读取yaml中的内容，和之前学过的文件读取有所不同
+            data = yaml.load(stream=file, Loader=yaml.FullLoader)
+            print('\n' + name + '文件下存储的数据：')
+            print(data)
+        print('读取数据结束！')
 
 
-# print(f"The directory '{directory_path}' contains {file_count} files.")
-# print(file_count)
+class Write_Yaml():
+    '''
+        写入yaml文件
+    '''
 
-li01 = []
-def list_files_in_directory(directory):
-    # 获取目录下的所有文件和子目录名
-    filenames = os.listdir(directory)
+    def __init__(self, dict_):
+        self.dict_ = dict_
 
-    # 打印文件名
-    for filename in filenames:
-        li01.append(filename)
-        # print(filename)
-
-
-# 指定要列出文件的目录
-# list_files_in_directory(directory_path)
-
-
-def read_yaml():
-    for name in li01:
-        file_path = './data/' + name
-        file = open(file_path, 'r', encoding='utf_8', )
-        # 读取yaml中的内容，和之前学过的文件读取有所不同
+    def write_yaml(self):
+        # w 是覆盖， a 是追加，r 是读取
+        with open('./data/data05.yaml', 'w', encoding='utf-8') as file:
+            yaml.dump(self.dict_, file, allow_unicode=True)
+        # 写入后读取写入的数据
+        file = open('./data/data05.yaml', 'r', encoding='utf-8')
         data = yaml.load(stream=file, Loader=yaml.FullLoader)
-        print(data)
+        print('\n' + str(data))
 
 
 if __name__ == '__main__':
-    read_yaml()
+    # 实例化一个对象，然后读取yaml文件数量、文件名和被解析后的文件内容
+    myRead_Yaml = Read_Yaml()
+    myRead_Yaml.count_files_in_directory()
+    myRead_Yaml.list_files_in_directory()
+    print('遍历后的目录文件：', end='')
+    print(Read_Yaml.li01)
+    myRead_Yaml.read_yaml()
+
+    # 创建一个实例
+    myclass = Write_Yaml(dict001)
+    # 调用实例方法
+    myclass.write_yaml()
